2cd7be829127e4117fc0f1f11f6889cf768282e2
ShivamGupta15 <infosys.shivam@gmail.com>
2018-10-07 17:26:03 +0530

    roll back

--- addition.py
+++ addition.py
@@ -1,13 +1,9 @@
 inp = input()
 inp1 = input()
-while inp.isdigit() is False and inp1.isdigit() is False:
-    print("Enter only digits")
-    inp = input()
-    inp1 = input()
-
-
 if inp.isdigit() and inp.isdigit():
     total = int(inp)+int(inp1)
     print("Total: ", total)
+else:
+    print("Enter only digits")
 
-
+print("this is my version")
