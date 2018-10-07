f554f5355c1a273941dc97ec575330b28f20b08a
ShivamGupta15 <infosys.shivam@gmail.com>
2018-10-07 17:10:11 +0530

    If user enters non digit input than it asks for iinput again and again

--- addition.py
+++ addition.py
@@ -1,7 +1,13 @@
 inp = input()
 inp1 = input()
+while inp.isdigit() is False and inp1.isdigit() is False:
+    print("Enter only digits")
+    inp = input()
+    inp1 = input()
+
+
 if inp.isdigit() and inp.isdigit():
     total = int(inp)+int(inp1)
     print("Total: ", total)
-else:
-    print("Enter only digits")
+
+
